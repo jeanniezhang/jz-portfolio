8633d2171252a957f4b3907e9423cc4f7f55087c
Jeannie Zhang <jeannie.zhang@withmantle.com>
2024-10-27 00:18:04 -0400

    more setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import './App.css'
+import "./App.css";
+import Sidebar from "./components/sidebar/Sidebar";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Services from "./components/services/Services";
+import Resume from "./components/resume/Resume";
+import Portfolio from "./components/portfolio/Portfolio";
+import Blog from "./components/blog/Blog";
+import Contact from "./components/contact/Contact";
 
 function App() {
   return (
     <div>
-      Webby webby website, lets goooo!
+      <Sidebar />
+      <main className="main-content">
+        <Home />
+        <About />
+        <Services />
+        <Resume />
+        <Portfolio />
+        <Blog />
+        <Contact />
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
